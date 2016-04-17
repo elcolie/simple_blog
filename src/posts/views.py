9e844f14b896a293a6c3d98fe169ce2080e02de5
sarit <cs.sarit@gmail.com>
2016-04-17 22:35:45 +0700

    warp git from first commit to 19/38 use render + simple query + template

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
-def post_home(request):
-    return HttpResponse("<h1>Hello Moon</h1>")
+def post_create(request):
+    return HttpResponse("<h1>Create Moon</h1>")
+
+def post_detail(request, id):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+    }
+    return render(request, "post_detail.html", context)
+
+
+def post_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "title" : "List",
+        "object_list" : queryset
+    }
+
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title" : "My user list"
+    #     }
+    # else:
+    #     context = {
+    #         "title" : "List"
+    #     }
+    return render(request, "index.html", context)
+
+def post_update(request):
+    return HttpResponse("<h1>Update Moon</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete Moon</h1>")
